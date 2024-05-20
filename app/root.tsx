6e6e20968882e6960e33b5328f02df7a743af789
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2024-05-20 17:30:51 +0300

    create dynamic route and load fake data

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,30 @@
-import type { LinksFunction } from '@remix-run/node';
+import { json, type LinksFunction } from '@remix-run/node';
 import {
     Form,
     Links,
     Meta,
     Scripts,
     ScrollRestoration,
+    Outlet,
+    useLoaderData,
+    Link,
 } from '@remix-run/react';
 import appStylesHref from './app.css';
+import { getContacts } from './data';
 
 export const links: LinksFunction = () => [
     { rel: 'stylesheet', href: appStylesHref },
 ];
 
+export async function loader() {
+    const contacts = await getContacts();
+
+    return json(contacts);
+}
+
 export default function App() {
+    const contacts = useLoaderData<typeof loader>();
+
     return (
         <html lang="en">
             <head>
@@ -47,15 +59,36 @@ export default function App() {
                         </Form>
                     </div>
                     <nav>
-                        <ul>
-                            <li>
-                                <a href={'/contacts/1'}>Your Name</a>
-                            </li>
-                            <li>
-                                <a href={'/contacts/2'}>Your Friend</a>
-                            </li>
-                        </ul>
+                        {contacts.length ? (
+                            <ul>
+                                {contacts.map((contact) => (
+                                    <li key={contact.id}>
+                                        <Link to={`contacts/${contact.id}`}>
+                                            {contact.first || contact.last ? (
+                                                <>
+                                                    {contact.first}{' '}
+                                                    {contact.last}
+                                                </>
+                                            ) : (
+                                                <i>No Name</i>
+                                            )}{' '}
+                                            {contact.favorite ? (
+                                                <span>★</span>
+                                            ) : null}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>
+                                <i>No contacts</i>
+                            </p>
+                        )}
                     </nav>
+                </div>
+
+                <div id="detail">
+                    <Outlet />
                 </div>
 
                 <ScrollRestoration />
